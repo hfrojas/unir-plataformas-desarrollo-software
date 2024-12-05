bb37a584b1c65fc0bd2a542e19ed7c685bb6859f
Harold Fernando Rojas Lopez <hrojas@hiberus.com>
2024-12-04 23:24:20 -0500

    Se crea el proyecto y Jframe para la toma de pedidos

---
+++ pharma-order-manager/src/main/java/com/pharmacare/pharma/order/manager/PharmaOrderManager.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.pharmacare.pharma.order.manager;
+
+import com.pharmacare.pharma.order.manager.view.PharmaOrderForm;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+/**
+ *
+ * @author hrojas
+ */
+public class PharmaOrderManager {
+
+    public static void main(String[] args) {
+        PharmaOrderForm pharmaOrderForm = new PharmaOrderForm();
+        pharmaOrderForm.setLocationRelativeTo(null);      
+        pharmaOrderForm.setVisible(true);
+    }
+}
