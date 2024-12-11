8660b7cec1b87419ba0e00848ecd9ca4cdfa3092
Harold Fernando Rojas Lopez <hrojas@hiberus.com>
2024-12-10 19:31:10 -0500

    Sincronizacion repositorio git

---
+++ Proyecto NetBeans13/pharma-order-manager/src/main/java/com/pharmacare/pharma/order/manager/PharmaOrderManager.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.pharmacare.pharma.order.manager;
+
+import com.pharmacare.pharma.order.manager.view.PharmaOrderForm;
+
+/**
+ *
+ * @author hrojas
+ */
+public class PharmaOrderManager {
+
+    public static void main(String[] args) {
+        PharmaOrderForm pharmaOrderForm = new PharmaOrderForm();
+        pharmaOrderForm.setLocationRelativeTo(null);      
+        pharmaOrderForm.setVisible(true);
+    }
+}
